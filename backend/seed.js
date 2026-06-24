05ab923f266f4c62652b6bc1ec47763b2397ce8d
WizardCarp85 <yashkumarnimje85@gmail.com>
2026-06-24 20:16:46 +0530

    Added bulk seeding for 200000 random products

--- backend/seed.js
+++ backend/seed.js
@@ -1,26 +1,65 @@
 // This files contains the script to seed the database with 200,000 products
 import pool from './db/db.js'
 
-let categories = ['Electronics','Books','Sports']
+let categories = ['Electronics','Books','Sports','Clothing','Furniture','Toys']
+
+const start = Date.now()
 
 async function seed(){
+    const TOTAL_PRODUCTS = 200000;
+    const BATCH_SIZE = 5000;
 
-    await pool.query(`
-        INSERT INTO products
-            (name,category,price,created_at,updated_at)
-        VALUES
-            ('Product1','Electronics',700,NOW(),NOW()),
-            ('Product2','Sports',200,NOW(),NOW()),
-            ('Product3','Electronics',500,NOW(),NOW()),
-            ('Product4','Sports',800,NOW(),NOW()),
-            ('Product5','Electronics',400,NOW(),NOW()),
-            ('Product6','Books',600,NOW(),NOW()),
-            ('Product7','Books',500,NOW(),NOW()),
-            ('Product8','Sports',900,NOW(),NOW()),
-            ('Product9','Electronics',400,NOW(),NOW()),
-            ('Product10','Books',200,NOW(),NOW())
-        `)
-    console.log("Inserted 10 products")
+    for(let batch=0; batch<TOTAL_PRODUCTS; batch += BATCH_SIZE){
+        const products = []
+
+        for(let i=1; i<=5000; i++){
+            products.push({
+                name : `Product ${batch+i}`,
+                category : categories[(Math.floor(Math.random()*categories.length))],
+                price : Math.floor(Math.random()*1000)+1
+            })
+        }
+
+
+        const placeholders = []
+        const values = []
+
+        products.forEach((product,index)=>{
+            const createdAt = new Date(
+                Date.now() - Math.random() * 365 * 24 * 60 * 60 * 1000
+            )
+
+            const updated_at = new Date(
+                createdAt.getTime() + Math.random() * 30 * 24 * 60 * 60 * 1000
+            )
+
+            const offset = index*5
+
+            placeholders.push(
+                `($${offset+1},$${offset+2},$${offset+3},$${offset+4},$${offset+5})`
+            )
+
+            values.push(
+                product.name,
+                product.category,
+                product.price,
+                createdAt,
+                updated_at
+            )
+        })
+
+        const query = `
+            INSERT INTO products
+                (name,category,price,created_at,updated_at)
+            VALUES
+                ${placeholders.join(',')}
+        `;
+
+        await pool.query(query,values)
+    }
+
+    console.log(`Completed in ${(Date.now()-start)/1000}s`);
+
     await pool.end();
 }
 
